b12f5c1ca75278a62398a8d0c29be1d8f659fbf6
Akadnt <nhatthong346@gmail.com>
2025-05-02 23:59:16 +0700

    Add identity user

--- Persistence/DatabaseContext.cs
+++ Persistence/DatabaseContext.cs
@@ -1,14 +1,13 @@
 using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Persistence
 {
-    public class DatabaseContext : DbContext
+    public class DatabaseContext(DbContextOptions options) : IdentityDbContext<User>(options)
     {
-        public DatabaseContext(DbContextOptions options) : base(options)
-        {
-        }
         public DbSet<Activity> Activities { get; set; }
     }
 }
